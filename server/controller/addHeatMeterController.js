e5bc08dd8f4b87a20f8ccaf38ce9df93efbd89c2
RomanLaptop <t89875041999@yandex.ru>
2023-02-02 22:24:53 +0300

    Changes to be committed: 	modified:   client/views/addHeatMeter.handlebars 	modified:   server/controller/addHeatMeterController.js

--- server/controller/addHeatMeterController.js
+++ server/controller/addHeatMeterController.js
@@ -1,9 +1,25 @@
+import Models from "../models/models.js";
 
 class AddHeatMeterController {
 
     async getPage (req, res) {
         try {
-            res.render("addHeatMeter")
+            const {idSection, objectId} = req.query
+            // Получим этажи
+            const flats = await Models.Flats.findAll({
+                where: {
+                    sectionId: idSection,
+                    objectId: objectId
+                },
+                raw: true,
+                include: Models.Floors
+            })
+
+            console.log(flats)
+
+            res.render("addHeatMeter", {
+                flats: flats
+            })
         } catch (e) {
             console.log(e)
         }
